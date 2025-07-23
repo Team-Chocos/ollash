7c9cd6d5049a42d65edd8374642b6d17569a68bf
Kedar Shinde <kedaryogesh.shinde2022@vitstudent.ac.in>
2025-07-23 15:51:03 +0530

    Added suuport for a yaml config, tested on linux, need to test on windows and macos

--- ollash/__main__.py
+++ ollash/__main__.py
@@ -1,17 +1,38 @@
+from pathlib import Path
+import yaml
+from platformdirs import user_config_dir
 import argparse
 from ollash.utils import ensure_ollama_ready
 from ollash.ollama_nl2bash import run_nl_to_bash
 
+def load_config():
+    config_path = Path(user_config_dir("ollash")) / "config.yaml"
+    if config_path.exists():
+        with open(config_path, "r")as f:
+            return yaml.safe_load(f) or {}
+    return {}
+
+
 def main():
+
+    config = load_config()
+
     parser = argparse.ArgumentParser(description="Ollash: Natural Language to Terminal Command")
     parser.add_argument("prompt", nargs="+", help="Your natural language instruction")
     parser.add_argument("--autostop", type=int, help="Time in seconds to auto-unload model")
     parser.add_argument("--model", default="llama3", help="Ollama model to use (default: llama3)")
     args = parser.parse_args()
 
+    print(args.autostop)
+
+    autostop = args.autostop if args.autostop else config.get("autostop")
+    model = args.model if args.model else config.get("model", "llama3")
+
+    print(autostop, model)
+
     if not args.prompt:
         print("❌ Please provide an instruction. Example:\nollash --autostop 300 list all files")
         return
 
     ensure_ollama_ready()
-    run_nl_to_bash(" ".join(args.prompt), autostop=args.autostop, model=args.model)
+    run_nl_to_bash(" ".join(args.prompt), autostop=autostop, model=model)
