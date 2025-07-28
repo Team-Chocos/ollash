7a4026f7f442b4f00028a61e7684634208a7b123
Sparsh <sparsh.riju@gmail.com>
2025-07-28 15:17:36 +0530

    project credentials updated

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 set -e  # Exit on error
 
+# WARNING: Don't commit this to git with your real token!
+PYPI_API_TOKEN="pypi-your-api-token-here"
+
 echo "Cleaning previous builds..."
 rm -rf build dist *.egg-info
 
@@ -9,6 +12,6 @@ echo "Building source and wheel distribution..."
 python setup.py sdist bdist_wheel
 
 echo "Uploading to PyPI..."
-twine upload dist/*
+twine upload dist/* --username __token__ --password "$PYPI_API_TOKEN"
 
 echo "Build and publish completed successfully!"
